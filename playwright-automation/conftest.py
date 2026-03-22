5f351d2e026a84697b618693e367c72cf2e5967d
Sami-qc <sami.mustafa.dev@gmail.com>
2026-03-22 12:04:02 +0200

    Fix conftest headless mode for CI

--- playwright-automation/conftest.py
+++ playwright-automation/conftest.py
@@ -1,11 +1,13 @@
 import pytest
+import os
 from playwright.sync_api import sync_playwright
 
 
 @pytest.fixture
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)  # 👈 هنا التحكم
+        headless = os.getenv("CI", "false").lower() == "true"
+        browser = p.chromium.launch(headless=headless)
         page = browser.new_page()
         yield page
         browser.close()
